e1199a4c9eb9253692ab5473f628a78e66284756
Harold Calderon <harold.calderon@sofka.com.co>
2023-02-21 22:28:04 -0500

    Creacion de Task login

--- src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
@@ -1,25 +1,39 @@
 package starter.stepdefinitions;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+import starter.login.Dologin;
+import starter.navigation.NavigateTo;
+
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 
 public class LoginStepDefinitions {
-    @Given("Sergey has an active account")
-    public void sergey_has_an_active_account() {
-        // Write code here that turns the phrase above into concrete actions
-      //  throw new io.cucumber.java.PendingException();
+
+    String name;
+    @Before
+    public void setTheStage() {
+        OnStage.setTheStage(new OnlineCast());
+    }
+
+    @Given("^(.*) has an active account")
+    public void sergey_has_an_active_account(String actor) {
+        this.name=name;
     }
     @When("he sends their valid credentials")
     public void he_sends_their_valid_credentials() {
-        // Write code here that turns the phrase above into concrete actions
-      //  throw new io.cucumber.java.PendingException();
+        theActorCalled(name).attemptsTo(
+                NavigateTo.theDuckDuckGoHomePage(),
+                Dologin.withCredentials("pepito","algo")
+        );
     }
     @Then("he should have access to manage his account")
     public void he_should_have_access_to_manage_his_account() {
         // Write code here that turns the phrase above into concrete actions
      //   throw new io.cucumber.java.PendingException();
     }
-
 
 }
